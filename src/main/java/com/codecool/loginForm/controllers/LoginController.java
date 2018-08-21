c87cc2ee7d31e578bb35bcb4e49cfebcbb06b21f
szymslow12 <szym.slow12@gmail.com>
2018-08-21 16:07:34 +0200

    add handleGet/handlePost/readAndParseFormData/parseFormData

--- src/main/java/com/codecool/loginForm/controllers/LoginController.java
+++ src/main/java/com/codecool/loginForm/controllers/LoginController.java
@@ -1,16 +1,18 @@
 package com.codecool.loginForm.controllers;
 
+import com.codecool.loginForm.models.User;
+import com.codecool.loginForm.models.UserRepository;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpCookie;
+import java.net.URLDecoder;
 import java.util.Scanner;
 import java.util.UUID;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class LoginController implements HttpHandler {
 
@@ -23,20 +25,54 @@ public class LoginController implements HttpHandler {
         this.httpExchange = httpExchange;
 
         String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
-
+        UserRepository userRepository = UserRepository.instance();
+        String method = httpExchange.getRequestMethod();
+        System.out.println(method + " - " + httpExchange.getRequestURI().toString());
 
         boolean isNewSession = checkCookies(cookieStr);
         try {
-            if (isNewSession) {
-                this.response = buildHtmlPage("index.html");
-                sendResponse();
+            if (method.equalsIgnoreCase("GET")) {
+                handleGet(isNewSession, cookieStr, userRepository);
             } else {
-                checkIfLogged();
+                handlePost(userRepository, cookieStr);
             }
         } catch (Exception err) {
             err.printStackTrace();
         }
 
+    }
+
+
+    private void handleGet(boolean isNewSession, String cookieStr, UserRepository userRepository) throws IOException {
+        if (isNewSession) {
+            this.response = buildHtmlPage("index.html");
+            sendResponse();
+        } else {
+            checkIfLogged(userRepository.getUserBySessionId(HttpCookie.parse(cookieStr).get(0).toString()));
+        }
+    }
+
+
+    private void handlePost(UserRepository userRepository, String cookieStr) throws IOException {
+        String sessionId = HttpCookie.parse(cookieStr).get(0).toString();
+
+        Map<String, String> formData = readAndParseFormData();
+        User user = userRepository.getUserByLogin(formData.get("login"));
+
+        if (user != null && user.getPassword().equals(formData.get("password"))) {
+            user.setSessionId(sessionId);
+            redirect("welcomePage");
+        }
+    }
+
+
+    private Map<String, String> readAndParseFormData() throws IOException {
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String formData = br.readLine();
+
+        System.out.println(formData);
+        return parseFormData(formData);
     }
 
 
@@ -92,13 +128,25 @@ public class LoginController implements HttpHandler {
         return stringBuilder.toString();
     }
 
-    private void checkIfLogged() throws IOException {
-        boolean isLogged = false;
-        if (isLogged) {
+    private void checkIfLogged(User user) throws IOException {
+        System.out.println(" " + user + "\u001B[0m");
+        if (user != null && user.isUserLogged()) {
             redirect("welcomePage");
         } else {
             this.response = buildHtmlPage("index.html");
             sendResponse();
         }
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new TreeMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            // We have to decode the value because it's urlencoded. see: https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 }
