0fc7e5286bcae7cd2ab817838d8b1b15e20ad883
szymslow12 <szym.slow12@gmail.com>
2018-08-21 11:31:17 +0200

    add LoginController with handle method

---
+++ src/main/java/com/codecool/loginForm/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.codecool.loginForm.controllers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class LoginController implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) {
+
+    }
+}
