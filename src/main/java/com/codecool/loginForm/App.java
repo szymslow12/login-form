49abecc762542c3887572384a1273c7659c148e7
szymslow12 <szym.slow12@gmail.com>
2018-08-21 11:30:56 +0200

    add HttpServer and needed imports, add new context -> /login

--- src/main/java/com/codecool/loginForm/App.java
+++ src/main/java/com/codecool/loginForm/App.java
@@ -1,7 +1,17 @@
 package com.codecool.loginForm;
 
+import com.codecool.loginForm.controllers.LoginController;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    public static void main(String[] args) throws Exception {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        httpServer.createContext("/login", new LoginController());
+
+        httpServer.setExecutor(null);
+
+        httpServer.start();
     }
 }
